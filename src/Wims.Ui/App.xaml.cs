536f71439f95777ae625e0d971b8df9c4f03711f
garyng <garyng.zhongbo@gmail.com>
2020-09-02 01:09:24 +0800

    feat: use microsoft di

--- src/Wims.Ui/App.xaml.cs
+++ src/Wims.Ui/App.xaml.cs
@@ -6,8 +6,9 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
-using ReactiveUI;
-using Splat;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Splat.Microsoft.Extensions.DependencyInjection;
 
 namespace Wims.Ui
 {
@@ -16,18 +17,37 @@ namespace Wims.Ui
 	/// </summary>
 	public partial class App : Application
 	{
+		private IHost _host;
+
+		public App()
+		{
+			var bootstrapper = new Bootstrapper();
+			_host = Host.CreateDefaultBuilder()
+				.ConfigureServices((context, services) =>
+				{
+					bootstrapper.ConfigureServices(context.HostingEnvironment, context.Configuration, services);
+				})
+				.Build();
+
+			// re-register MS DI with Splat
+			_host.Services.UseMicrosoftDependencyResolver();
+		}
+
 		protected override void OnStartup(StartupEventArgs e)
 		{
-			Locator.CurrentMutable.Register(() => new MainViewModel());
-			Locator.CurrentMutable.RegisterViewsForViewModels(typeof(App).Assembly);
-
-
+			_host.StartAsync();
 			var mainView = new MainView
 			{
-				ViewModel = Locator.Current.GetService<MainViewModel>()
+				ViewModel = _host.Services.GetRequiredService<MainViewModel>()
 			};
 			mainView.Closed += (o, args) => Shutdown();
 			mainView.Show();
+		}
+
+		protected override async void OnExit(ExitEventArgs e)
+		{
+			await _host.StopAsync();
+			_host.Dispose();
 		}
 	}
 }
---
+++ src/Wims.Ui/Bootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ReactiveUI;
+using Splat;
+using Splat.Microsoft.Extensions.DependencyInjection;
+
+namespace Wims.Ui
+{
+	public class Bootstrapper
+	{
+		public void ConfigureServices(IHostEnvironment environment, IConfiguration configuration,
+			IServiceCollection services)
+		{
+			var splat = ConfigureSplat(services);
+
+
+			services.AddSingleton<MainViewModel>();
+
+		}
+
+		private IMutableDependencyResolver ConfigureSplat(IServiceCollection services)
+		{
+			services.UseMicrosoftDependencyResolver();
+
+			var splat = Locator.CurrentMutable;
+			splat.InitializeSplat();
+			splat.InitializeReactiveUI();
+			splat.RegisterViewsForViewModels(typeof(App).Assembly);
+
+			return splat;
+		}
+	}
+}
