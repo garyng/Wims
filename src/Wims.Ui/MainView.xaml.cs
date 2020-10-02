c4a6fe0971f5d08063620df328503ef779414dea
garyng <garyng.zhongbo@gmail.com>
2020-10-02 22:24:53 +0800

    fix: remove window centering as the window will jump

--- src/Wims.Ui/MainView.xaml.cs
+++ src/Wims.Ui/MainView.xaml.cs
@@ -1,25 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
 using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ReactiveUI;
 using Wims.Core.Dto;
-using Application = System.Windows.Application;
-using Void = GaryNg.Utils.Void.Void;
 
 namespace Wims.Ui
 {
@@ -67,31 +51,6 @@ namespace Wims.Ui
 					.DisposeWith(d);
 
 				this.ViewModel.LoadShortcuts.Execute().Subscribe();
-
-				// move window to center on results first load
-				// https://stackoverflow.com/a/32599760
-				this.ResultsContainer
-					.Events().SizeChanged
-					.Where(s => s.NewSize.Height > 0)
-					.Subscribe(e =>
-					{
-						var mainWindow = Application.Current.MainWindow;
-						var hwnd = new WindowInteropHelper(mainWindow).Handle;
-						var currentMonitor = Screen.FromHandle(hwnd);
-
-						var source = PresentationSource.FromVisual(mainWindow);
-						double dpiScaling = source?.CompositionTarget?.TransformFromDevice.M11 ?? 1;
-
-						var workArea = currentMonitor.WorkingArea;
-						var width = (int)Math.Floor(workArea.Width * dpiScaling);
-						var height = (int)Math.Floor(workArea.Height * dpiScaling);
-
-						mainWindow.Left = (((width - (mainWindow.ActualWidth * dpiScaling)) / 2) +
-										   (workArea.Left * dpiScaling));
-						mainWindow.Top = (((height - (mainWindow.ActualHeight * dpiScaling)) / 2) +
-										  (workArea.Top * dpiScaling));
-					})
-					.DisposeWith(d);
 			});
 		}
 
