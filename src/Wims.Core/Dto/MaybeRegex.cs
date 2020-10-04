1c94ba7e0fd6dc191eb1b99201bc5fa51c0e6a30
garyng <garyng.zhongbo@gmail.com>
2020-10-04 10:15:15 +0800

    feat: handle string pattern that might be regex

---
+++ src/Wims.Core/Dto/MaybeRegex.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Wims.Core.Dto
+{
+	/// <summary>
+	/// If string starts and ends with /, then use Regex,
+	/// else plain string comparison is used (case insensitive).
+	/// </summary>
+	public class MaybeRegex
+	{
+		private Regex _regex;
+		private string _pattern;
+
+		public bool IsRegex => _regex != null;
+
+		public MaybeRegex(string pattern)
+		{
+			var match = Regex.Match(pattern, @"^/(.*)/$");
+			if (match.Success)
+			{
+				_regex = new Regex(match.Groups[1].Value, RegexOptions.Compiled);
+			}
+			else
+			{
+				_pattern = pattern;
+			}
+		}
+
+		public bool IsMatch(string input)
+		{
+			return _regex?.IsMatch(input)
+			       ?? _pattern.Equals(input, StringComparison.InvariantCultureIgnoreCase);
+		}
+	}
+}
---
+++ src/Wims.Tests/MaybeRegexTests.cs
@@ -0,0 +1,63 @@
+using FluentAssertions;
+using NUnit.Framework;
+using Wims.Core.Dto;
+
+namespace Wims.Tests
+{
+	public class MaybeRegexTests
+	{
+		[TestCase("/[a-z]/")]
+		[TestCase("/http://www.google.com/")]
+		public void Should_BeRegex_IfIsRegex(string str)
+		{
+			// Arrange
+			var pattern = new MaybeRegex(str);
+
+			// Act
+
+			// Assert
+			pattern.IsRegex.Should().BeTrue();
+		}
+
+		[TestCase("a-z")]
+		[TestCase("http://www.google.com/")]
+		public void Should_BeString_IfNotRegex(string str)
+		{
+			// Arrange
+			var pattern = new MaybeRegex(str);
+
+			// Act
+
+
+			// Assert
+			pattern.IsRegex.Should().BeFalse();
+		}
+
+		[TestCase("/[a-z]{2}/", "az", true)]
+		[TestCase("/[a-z]{2}/", "123", false)]
+		public void Should_MatchWithRegex_IfIsRegex(string pattern, string input, bool expected)
+		{
+			// Arrange
+			var p = new MaybeRegex(pattern);
+
+			// Act
+			var match = p.IsMatch(input);
+
+			// Assert
+			match.Should().BeTrue();
+		}
+
+		[TestCase("abc", "abc", true)]
+		[TestCase("abc", "Abc", false)]
+		public void Should_MatchIgnoreCase_IfNotRegex(string pattern, string input, bool expected)
+		{
+			var p = new MaybeRegex(pattern);
+
+			// Act
+			var match = p.IsMatch(input);
+
+			// Assert
+			match.Should().BeTrue();
+		}
+	}
+}
