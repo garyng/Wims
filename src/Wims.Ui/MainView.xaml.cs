0bb7cf3bacf8d4a08b88a9193fe59fb9a7d4b028
garyng <garyng.zhongbo@gmail.com>
2020-10-04 15:05:32 +0800

    refactor: extract methods for main view

--- src/Wims.Ui/MainView.xaml.cs
+++ src/Wims.Ui/MainView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Windows;
@@ -12,6 +13,7 @@ using ReactiveUI;
 using Wims.Core.Dto;
 using NHotkey.Wpf;
 using Application = System.Windows.Application;
+using Void = GaryNg.Utils.Void.Void;
 
 namespace Wims.Ui
 {
@@ -72,36 +74,6 @@ namespace Wims.Ui
 						})
 					.DisposeWith(d);
 
-				// hotkey for switching query mode
-				Observable.FromEventPattern<HotkeyEventArgs>(
-						add =>
-							HotkeyManager.Current.AddOrReplace("Switch Query Mode", Key.Back, ModifierKeys.Shift, add),
-						remove => HotkeyManager.Current.Remove("Switch Query Mode"))
-					.Select(_ => ViewModel.QueryMode switch
-					{
-						QueryModes.Text => QueryModes.Keys,
-						QueryModes.Keys => QueryModes.Text,
-						_ => throw new ArgumentOutOfRangeException()
-					})
-					.Subscribe(mode => { this.ViewModel.QueryMode = mode; });
-
-
-				// auto focus on the search box when switching query mode
-				this.WhenAnyValue(v => v.ViewModel.QueryMode)
-					.Select(m => m switch
-					{
-						QueryModes.Text => TextQuery,
-						QueryModes.Keys => KeysQuery,
-						_ => throw new ArgumentOutOfRangeException()
-					})
-					.Subscribe(textbox => { textbox.Focus(); });
-
-				this.Exit
-					.Events()
-					.Click
-					.Subscribe(_ => Application.Current.Shutdown())
-					.DisposeWith(d);
-
 				this.BindCommand(ViewModel, vm => vm.LoadShortcuts, v => v.Reload)
 					.DisposeWith(d);
 
@@ -109,17 +81,102 @@ namespace Wims.Ui
 				// load all shortcuts
 				this.ViewModel.LoadShortcuts.Execute().Subscribe();
 
+				this.Exit
+					.Events()
+					.Click
+					.Subscribe(_ => Application.Current.Shutdown())
+					.DisposeWith(d);
+
 				this.TrayIcon
 					.DisposeWith(d);
 
-				Hook.GlobalEvents()
-					.DisposeWith(d)
-					.OnCombination(new Dictionary<Combination, Action>
-					{
-						{Combination.TriggeredBy(Keys.Escape).With(Keys.LWin), ActivateWindow}
-					});
+				AutoFocusSearchBox(d);
+				AppHotKey(d);
+				HideOnBackspace(d);
+				GlobalHotKey(d);
 			});
 		}
+
+		private void GlobalHotKey(CompositeDisposable d)
+		{
+			Hook.GlobalEvents()
+				.DisposeWith(d)
+				.OnCombination(new Dictionary<Combination, Action>
+				{
+					{Combination.TriggeredBy(Keys.Escape).With(Keys.LWin), ActivateWindow}
+				});
+		}
+
+		private void AppHotKey(CompositeDisposable d)
+		{
+			// switch query mode
+			Observable.FromEventPattern<HotkeyEventArgs>(
+					add =>
+						HotkeyManager.Current.AddOrReplace("Switch Query Mode", Key.Back, ModifierKeys.Shift, add),
+					remove => HotkeyManager.Current.Remove("Switch Query Mode"))
+				.Select(_ => ViewModel.QueryMode switch
+				{
+					QueryModes.Text => QueryModes.Keys,
+					QueryModes.Keys => QueryModes.Text,
+					_ => throw new ArgumentOutOfRangeException()
+				})
+				.Subscribe(mode => { this.ViewModel.QueryMode = mode; })
+				.DisposeWith(d);
+		}
+
+		private void HideOnBackspace(CompositeDisposable d)
+		{
+			var contexts = this.WhenAnyValue(v => v.ViewModel.Context);
+			var keys = Observable.FromEvent(add => this.KeysQuery.OnWillBackspace += add,
+					remove => this.KeysQuery.OnWillBackspace -= remove)
+				.Where(_ => string.IsNullOrEmpty(this.KeysQuery.Text))
+				.Select(_ => Void.Default);
+
+			var texts = this.TextQuery
+				.Events()
+				.PreviewKeyDown
+				.Where(e => e.Key == Key.Back)
+				.Where(_ => string.IsNullOrEmpty(this.TextQuery.Text))
+				.Select(_ => Void.Default);
+
+			keys.Merge(texts)
+				.WithLatestFrom(contexts, (_, c) => c)
+				.ObserveOnDispatcher()
+				.Subscribe(async c =>
+				{
+					if (c != null)
+					{
+						await ViewModel.RemoveContext.Execute();
+					}
+					else
+					{
+						Hide();
+					}
+				})
+				.DisposeWith(d);
+		}
+
+		private void AutoFocusSearchBox(CompositeDisposable d)
+		{
+			// auto focus on the textbox when switching query mode
+			this.WhenAnyValue(v => v.ViewModel.QueryMode)
+				.Select(m => m switch
+				{
+					QueryModes.Text => TextQuery,
+					QueryModes.Keys => KeysQuery,
+					_ => throw new ArgumentOutOfRangeException()
+				})
+				.Subscribe(textbox => { textbox.Focus(); })
+				.DisposeWith(d);
+		}
+
+		private async void ActivateWindow()
+		{
+			await ViewModel.RefreshContext.Execute();
+			Show();
+			Activate();
+		}
+
 
 		private void OnDrag(object sender, MouseButtonEventArgs e)
 		{
@@ -132,13 +189,6 @@ namespace Wims.Ui
 		private void OnTrayIconDoubleClick(object sender, RoutedEventArgs e)
 		{
 			ActivateWindow();
-		}
-
-		private async void ActivateWindow()
-		{
-			await ViewModel.RefreshContext.Execute();
-			Show();
-			Activate();
 		}
 
 		private void OnDeactivated(object? sender, EventArgs e)
