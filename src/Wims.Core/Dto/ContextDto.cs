b55541e686ff2c21d2eb46faa69ec168c6f3ceee
garyng <garyng.zhongbo@gmail.com>
2020-10-04 12:26:52 +0800

    feat: add equality

--- src/Wims.Core/Dto/ContextDto.cs
+++ src/Wims.Core/Dto/ContextDto.cs
@@ -1,9 +1,31 @@
-﻿namespace Wims.Core.Dto
+﻿using System;
+
+namespace Wims.Core.Dto
 {
-	public class ContextDto
+	public class ContextDto : IEquatable<ContextDto>
 	{
 		public string Name { get; set; }
 		public string Icon { get; set; }
 		public MatchDto Match { get; set; }
+
+		public bool Equals(ContextDto other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return Name == other.Name;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+			return Equals((ContextDto)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return (Name != null ? Name.GetHashCode() : 0);
+		}
 	}
 }
